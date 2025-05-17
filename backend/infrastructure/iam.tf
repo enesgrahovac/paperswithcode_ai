d1420fedfbac031ec0d25771e92b3891b58826a3
Enes Grahovac <enesxgrahovac@gmail.com>
2025-05-17 11:06:37 -0400

    adding iam.tf

---
+++ backend/infrastructure/iam.tf
@@ -0,0 +1,23 @@
+# IAM role that all of your Lambda functions will assume
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach the managed policy that lets Lambdas write logs, access X-Ray, etc.
+resource "aws_iam_role_policy_attachment" "lambda_basic_exec" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
